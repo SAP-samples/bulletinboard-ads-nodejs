1ee593cb17933df471043dbb8db681a85889daea
Sebastian Krieghoff <sebastian.krieghoff@sap.com>
2020-03-05 11:53:20 +0100

    defaults in main.js

--- js/main.js
+++ js/main.js
@@ -4,18 +4,20 @@ const ExpressServer = require('./express-server')
 const PostgresAdsService = require('./postgres-ads-service')
 const ReviewsClient = require('./reviews-client')
 const VCAP_SERVICES = process.env.VCAP_SERVICES
-const REVIEWS_HOST = process.env.REVIEWS_HOST
+const REVIEWS_HOST = process.env.REVIEWS_HOST || 'http://localhost:9090'
 const REVIEWS_HOST_INTERNAL = process.env.REVIEWS_HOST_INTERNAL || REVIEWS_HOST;
-let DB_CONNECTION_URI  = process.env.POSTGRES_URI
+const DB_CONNECTION_URI_DEFAULT  = 'postgres://postgres@localhost:5432/postgres'
+const PORT_DEFAULT = 8080
+let dbConnectionUriVCAP
 
 if (VCAP_SERVICES) {
     const vcapServices = JSON.parse(VCAP_SERVICES)
-    DB_CONNECTION_URI = vcapServices.postgresql[0].credentials.uri
+    dbConnectionUriVCAP = vcapServices.postgresql[0].credentials.uri
 }
 
 const server = new ExpressServer(
-  new PostgresAdsService(DB_CONNECTION_URI),
+  new PostgresAdsService(dbConnectionUriVCAP || DB_CONNECTION_URI_DEFAULT),
   new ReviewsClient(REVIEWS_HOST_INTERNAL),
   REVIEWS_HOST
 )
-server.start(process.env.PORT)
+server.start(process.env.PORT || PORT_DEFAULT)
