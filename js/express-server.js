fc7a33a3547d87010a2553c7a5c9305e4d7be3fd
Jens Keller <jens.keller@sap.com>
2021-10-19 11:31:22 +0800

    fix error handling bug

--- js/express-server.js
+++ js/express-server.js
@@ -7,6 +7,7 @@ const CREATED = 201
 const NO_CONTENT = 204
 const BAD_REQUEST = 400
 const NOT_FOUND = 404
+const INTERNAL_SERVER_ERROR = 500
 
 function ExpressServer(adsService, reviewsClient, reviewsHost, defaultLogger) {
     let httpServer
