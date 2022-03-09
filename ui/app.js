f6130989b7f198feb289daa9a255b18ae33e3ed9
Jens Keller <jens.keller@sap.com>
2022-03-09 18:08:29 +0800

    indent 2

--- ui/app.js
+++ ui/app.js
@@ -6,25 +6,25 @@ import AdDetails from './ad-details.js'
 
 // FIXME changing the hash directly when already on the details view will not load the data of the new ad
 const App = function (props) {
-    const [state, setState] = useState({ adId: '', isCreate: false })
+  const [state, setState] = useState({ adId: '', isCreate: false })
 
-    useEffect(() => {
-        setStateFromUrl()
-        addEventListener('hashchange', setStateFromUrl)
-        return () => removeEventListener('hashchange', setStateFromUrl)
-    }, [])
+  useEffect(() => {
+    setStateFromUrl()
+    addEventListener('hashchange', setStateFromUrl)
+    return () => removeEventListener('hashchange', setStateFromUrl)
+  }, [])
 
-    const setStateFromUrl = () => {
-        const indexShow = location.hash.indexOf('#/show/')
-        const adId = (indexShow > -1) ? location.hash.substring(indexShow + 7) : ''
-        const isCreate = location.hash.indexOf('#/new') > -1
-        setState({ adId, isCreate })
-    }
+  const setStateFromUrl = () => {
+    const indexShow = location.hash.indexOf('#/show/')
+    const adId = (indexShow > -1) ? location.hash.substring(indexShow + 7) : ''
+    const isCreate = location.hash.indexOf('#/new') > -1
+    setState({ adId, isCreate })
+  }
 
-    const showDetails = state.adId || state.isCreate
-    return showDetails
-        ? html`<${AdDetails} client=${props.client} adId=${state.adId} isCreate=${state.isCreate} />`
-        : html`<${AdsOverview} client=${props.client} />`
+  const showDetails = state.adId || state.isCreate
+  return showDetails
+    ? html`<${AdDetails} client=${props.client} adId=${state.adId} isCreate=${state.isCreate} />`
+    : html`<${AdsOverview} client=${props.client} />`
 }
 
 export default App
